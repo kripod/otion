01d8e220af3851151f0828bee1969e4d7cfe9c0b
Kristóf Poduszló <kripod@protonmail.com>
2020-05-07 00:44:31 +0200

    fix(react-otion): style element SSR

--- packages/react-otion/src/server.tsx
+++ packages/react-otion/src/server.tsx
@@ -3,5 +3,8 @@ import * as React from 'react';
 export * from 'otion/server';
 
 export function getStyleElements(rules: string[]): JSX.Element[] {
-  return [<style id="__otion">{rules.join('')}</style>];
+  return [
+    // eslint-disable-next-line react/no-danger
+    <style id="__otion" dangerouslySetInnerHTML={{ __html: rules.join('') }} />,
+  ];
 }
