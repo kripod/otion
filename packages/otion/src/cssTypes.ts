1a572177904d0b22dd98b17838b147afc27950b0
Kristóf Poduszló <kripod@protonmail.com>
2020-05-11 12:47:15 +0200

    revert: remove remains of global styling

--- packages/otion/src/cssTypes.ts
+++ packages/otion/src/cssTypes.ts
@@ -1,38 +1,9 @@
 import * as CSS from 'csstype';
 
-export type MarginAtRules = {
-  [pageMargin in
-    | '@top-left-corner'
-    | '@top-left'
-    | '@top-center'
-    | '@top-right'
-    | '@top-right-corner'
-    | '@right-top'
-    | '@right-middle'
-    | '@right-bottom'
-    | '@bottom-right-corner'
-    | '@bottom-right'
-    | '@bottom-center'
-    | '@bottom-left'
-    | '@bottom-left-corner'
-    | '@left-bottom'
-    | '@left-middle'
-    | '@left-top']?: CSSProperties; // TODO: PageMarginCSSProperties
-};
-
-export type PageFallback =
-  | CSSProperties // TODO: PageCSSProperties
-  | MarginAtRules
-  | {
-      [pseudo in ' :left' | ' :right' | ' :first' | ' :blank']?: MarginAtRules;
-    };
-
 export type CSSProperties = CSS.PropertiesFallback<string | number>;
 
-export type CSSRules = CSSProperties;
-
-export type CSSStyleRules = CSSRules &
-  { [pseudo in CSS.SimplePseudos]?: CSSRules };
+export type CSSStyleRules = CSSProperties &
+  { [pseudo in CSS.SimplePseudos]?: CSSProperties };
 
 export interface CSSGroupingRules {
   '@media'?: {
@@ -43,15 +14,8 @@ export interface CSSGroupingRules {
   };
 }
 
+export type ScopedCSSRules = CSSStyleRules & CSSGroupingRules;
+
 export type CSSKeyframeRules =
   | { [time in 'from' | 'to']?: CSSProperties }
   | { [time: string]: CSSProperties };
-
-export type ScopedCSSRules = CSSStyleRules & CSSGroupingRules;
-
-export type GlobalCSSRules = ScopedCSSRules & {
-  '@font-face'?: CSS.FontFaceFallback | CSS.FontFaceFallback[];
-  '@import'?: string | string[];
-  '@namespace'?: string | string[];
-  '@page'?: PageFallback;
-};
