a533408818cb19ea94f00160b6119d570aaf624e
Kristóf Poduszló <kripod@protonmail.com>
2020-05-17 01:15:54 +0200

    feat(example-nextjs): support SSR

---
+++ packages/example-nextjs/src/pages/_document.tsx
@@ -0,0 +1,24 @@
+import Document, { DocumentContext, DocumentInitialProps } from 'next/document';
+import { setup } from 'otion';
+import {
+  filterOutUnusedRules,
+  getStyleElement,
+  VirtualInjector,
+} from 'react-otion/server';
+
+import options from '../../otion.config';
+
+export default class MyDocument extends Document {
+  static async getInitialProps({
+    renderPage,
+  }: DocumentContext): Promise<DocumentInitialProps> {
+    const injector = VirtualInjector();
+    setup({ ...options, injector });
+
+    const page = await renderPage();
+    return {
+      ...page,
+      styles: getStyleElement(filterOutUnusedRules(injector, page.html)),
+    };
+  }
+}
