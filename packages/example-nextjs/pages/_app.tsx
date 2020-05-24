0ed154d0e44a60b0da3ae32a1ea268a238df008a
Kristóf Poduszló <kripod@protonmail.com>
2020-05-24 16:59:16 +0200

    fix(example-nextjs): set up otion only once

--- packages/example-nextjs/pages/_app.tsx
+++ packages/example-nextjs/pages/_app.tsx
@@ -1,12 +1,17 @@
 import { AppProps } from "next/app";
 import { hydrate, setup } from "otion";
 import * as React from "react";
+import { useRef } from "react";
 
 import options from "../otion.config";
 
 export default function MyApp({ Component, pageProps }: AppProps): JSX.Element {
+	const isOtionSetupNeeded = useRef(true);
 	if (typeof window !== "undefined") {
-		setup(options);
+		if (isOtionSetupNeeded.current) {
+			setup(options);
+			isOtionSetupNeeded.current = false;
+		}
 		hydrate();
 	}
 
