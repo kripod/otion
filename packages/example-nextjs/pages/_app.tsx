d17ce1c5f0140ce7b2194132cb7016bdd86f4e73
Kristóf Poduszló <kripod@protonmail.com>
2020-05-24 17:01:50 +0200

    revert: "fix(example-nextjs): set up otion only once"
    This reverts commit 0ed154d0e44a60b0da3ae32a1ea268a238df008a.

--- packages/example-nextjs/pages/_app.tsx
+++ packages/example-nextjs/pages/_app.tsx
@@ -1,17 +1,12 @@
 import { AppProps } from "next/app";
 import { hydrate, setup } from "otion";
 import * as React from "react";
-import { useRef } from "react";
 
 import options from "../otion.config";
 
 export default function MyApp({ Component, pageProps }: AppProps): JSX.Element {
-	const isOtionSetupNeeded = useRef(true);
 	if (typeof window !== "undefined") {
-		if (isOtionSetupNeeded.current) {
-			setup(options);
-			isOtionSetupNeeded.current = false;
-		}
+		setup(options);
 		hydrate();
 	}
 
