d47f29756b1b59a359c493b17925f3840d347fe5
Tiago Souza <33164463+etc-tiago@users.noreply.github.com>
2020-07-15 18:52:13 -0300

    docs: add basic CRA example (#26)
    * chore: add basic CRA example
    
    * chore: add lint and type-check scripts
    
    * chore: add cra public folder
    
    Co-authored-by: Tiago Souza <tiago@explorethecuriosity.com>

---
+++ packages/example-cra/src/index.tsx
@@ -0,0 +1,13 @@
+import "./index.css";
+
+import * as React from "react";
+import * as ReactDOM from "react-dom";
+
+import App from "./App";
+
+ReactDOM.render(
+	<React.StrictMode>
+		<App />
+	</React.StrictMode>,
+	document.getElementById("root"),
+);
