72d1c79f04922a34fcb5a1874426b788019b36ae
Kristóf Poduszló <kripod@protonmail.com>
2020-07-22 14:35:11 +0200

    fix(example-nextjs): hydrate styles only once

--- packages/example-nextjs/pages/_app.tsx
+++ packages/example-nextjs/pages/_app.tsx
@@ -4,11 +4,11 @@ import * as React from "react";
 
 import options from "../otion.config";
 
-export default function MyApp({ Component, pageProps }: AppProps): JSX.Element {
-	if (typeof window !== "undefined") {
-		setup(options);
-		hydrate();
-	}
+if (typeof window !== "undefined") {
+	setup(options);
+	hydrate();
+}
 
+export default function MyApp({ Component, pageProps }: AppProps): JSX.Element {
 	return <Component {...pageProps} />;
 }
