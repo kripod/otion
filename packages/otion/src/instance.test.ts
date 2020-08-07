fdbac4705218df4d3501cf5161b56586908ecc84
Kristóf Poduszló <kripod@protonmail.com>
2020-08-07 18:55:08 +0200

    chore: refactor instance tests

--- packages/otion/src/instance.test.ts
+++ packages/otion/src/instance.test.ts
@@ -4,21 +4,6 @@ import {
 	stringifyDeclarationList,
 } from "./instance";
 
-test("maps a simple CSS rule to a stable class name", () => {
-	const c1 = css({ color: "red" });
-	const c2 = css({ color: "red" });
-
-	expect(c1).toBe(c2);
-	expect(c1).toBe(c1.trim());
-});
-
-test("maps multiple simple CSS rules to a set of stable class names", () => {
-	const c1 = css({ color: "red", background: "green" });
-	const c2 = css({ background: "green", color: "red" });
-
-	expect(new Set(c1.split(" "))).toEqual(new Set(c2.split(" ")));
-});
-
 test("auto-prefixes a CSS property–value pair", () => {
 	const c1 = stringifyDeclaration("user-select", "none");
 
@@ -35,10 +20,12 @@ test("postfixes numeric CSS value with a unit if necessary", () => {
 
 test("doesn't postfix unitless CSS values with a unit", () => {
 	const c1 = stringifyDeclaration("line-height", 1.5);
-	const c2 = stringifyDeclaration("padding", 0);
+	const c2 = stringifyDeclaration("padding", "16px");
+	const c3 = stringifyDeclaration("padding", 0);
 
 	expect(c1).toBe("line-height:1.5");
-	expect(c2).toBe("padding:0");
+	expect(c2).toBe("padding:16px");
+	expect(c3).toBe("padding:0");
 });
 
 test("stringifies fallback values in order", () => {
@@ -48,4 +35,19 @@ test("stringifies fallback values in order", () => {
 	]);
 
 	expect(c1).toBe("justify-content:space-around;justify-content:space-evenly");
+});
+
+test("maps a simple CSS rule to a stable class name", () => {
+	const c1 = css({ color: "red" });
+	const c2 = css({ color: "red" });
+
+	expect(c1).toBe(c2);
+	expect(c1).toBe(c1.trim());
+});
+
+test("maps multiple simple CSS rules to a set of stable class names", () => {
+	const c1 = css({ color: "red", background: "green" });
+	const c2 = css({ background: "green", color: "red" });
+
+	expect(new Set(c1.split(" "))).toEqual(new Set(c2.split(" ")));
 });
